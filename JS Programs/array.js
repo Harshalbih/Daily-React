c48c815c3d97d94bbdec9e321805e3f83e89af45
Harshal <bihadeharsh@gmail.com>
2023-01-31 17:05:54 +0530

    updated js

--- JS Programs/array.js
+++ JS Programs/array.js
@@ -13,7 +13,6 @@ function dupl(array) {
 }
 console.log(dupl(array));
 
-
 //Unique Item in array
 let a = [1, 3, 5, 7, 1, 3, 5, 7, 9];
 
@@ -24,52 +23,58 @@ console.log(lonelyinteger(a));
 
 //Find duplicate items from array
 let array = [1, 3, 5, 7, 1, 3, 5, 7, 9];
-const dups= array.filter((ele,index,arr)=> arr.indexOf(ele)!==index);
+const dups = array.filter((ele, index, arr) => arr.indexOf(ele) !== index);
 console.log(dups);
 
 //Max number in array
 let array = [1, 3, 5, 7, 1, 3, 5, 7, 9];
-let maxnum = array.reduce((pre, curr)=> curr>pre? curr: pre);
+let maxnum = array.reduce((pre, curr) => (curr > pre ? curr : pre));
 console.log(maxnum);
 
-//or 
-let maxnum  = Math.max(...array);
+//or
+let maxnum = Math.max(...array);
 console.log(maxnum);
 
 //Second largest value im array
 let array = [1, 3, 5, 7, 1, 3, 5, 7, 9];
 let maxnum = Math.max(...array);
 let index = array.indexOf(maxnum);
-array.splice(index,1)
-let newmax = Math.max(...array)
-console.log(newmax)
+array.splice(index, 1);
+let newmax = Math.max(...array);
+console.log(newmax);
 
 //Missing value in an array
 let array = [1, 3, 5, 7, 1, 3, 5, 7, 9];
-let misarray=[];
+let misarray = [];
 let maxnum = Math.max(...array);
 let minnum = Math.min(...array);
-for(i=minnum; i<maxnum;i++){
-  if(array.indexOf(i)<0){
-    misarray.push(i)
+for (i = minnum; i < maxnum; i++) {
+  if (array.indexOf(i) < 0) {
+    misarray.push(i);
   }
 }
 console.log(misarray);
 
 //Even odd
 let array = [1, 2, 4, 3, 5, 7, 1, 3, 5, 7, 9];
-const odd= array.filter((item)=> item%2 ===1);
+const odd = array.filter((item) => item % 2 === 1);
 console.log(odd);
-const even= array.filter((item)=> item%2 ===0);
+const even = array.filter((item) => item % 2 === 0);
 console.log(even);
 
 //Intersection of two arrays
-let arr1 = [2,4,4,6,8,9]
-let arr2 = [2,4,7,8,8,5]
+let arr1 = [2, 4, 4, 6, 8, 9];
+let arr2 = [2, 4, 7, 8, 8, 5];
 
-let intersect= arr1.filter((ele)=> {
-    return arr2.includes(ele);
-})
-console.log([...new Set(intersect)])
+let intersect = arr1.filter((ele) => {
+  return arr2.includes(ele);
+});
+console.log([...new Set(intersect)]);
 
+//Union of array
+let arr1 = [2, 4, 4, 6, 8, 9];
+let arr2 = [2, 4, 7, 8, 8, 5];
 
+let union = [...arr1, ...arr2];
+
+console.log([...new Set(union)]);
